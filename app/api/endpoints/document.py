039b8b9c3726797ae395bb2866a1ab22135d8f92
Francisco Gandala <gandala@discente.ufg.br>
2025-11-22 19:21:59 +0000

    feat: finalizando a feature de ingestão dos documentos e criação das chunks

--- app/api/endpoints/document.py
+++ app/api/endpoints/document.py
@@ -1,7 +1,32 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException, status
+from langchain_huggingface import HuggingFaceEmbeddings
 from app.schemas.document import RetrievalRequest, RetrievalResponse, IngestionResponse, DocumentResponse
+from app.services.ingestion import IngestionService
+from app.core.config import settings
+
+
+try:
+    global_embeddings = HuggingFaceEmbeddings(
+        model_name=settings.EMBEDDING_MODEL_NAME,
+        model_kwargs={
+            'device': 'cpu', 
+            'trust_remote_code': True,
+            'token': settings.HF_TOKEN
+        },
+        encode_kwargs={'normalize_embeddings': True}
+    )
+
+    ingestion_service = IngestionService(embeddings=global_embeddings)
+    
+    print("Modelo carregado e serviços prontos!")
+
+except Exception as e:
+    print(f"Erro ao carregar modelo: {e}")
+    raise e
+
 
 router = APIRouter()
+
 
 @router.post("/ingest", response_model=IngestionResponse,status_code=status.HTTP_201_CREATED)
 async def ingest_document(file: UploadFile = File(...)):
@@ -11,14 +36,26 @@ async def ingest_document(file: UploadFile = File(...)):
     if(file.content_type not in ["application/pdf","text/plain"]):
         raise HTTPException(status_code=400, detail="Apenas arquivos PDF ou TXT são permitidos.")
     
+    try:
+        
+        number_of_chunks = await ingestion_service.process_document(file)
 
-    return IngestionResponse(
+        return IngestionResponse(
         filename=file.filename,
         status="success",
-        chunks_created=10,
+        chunks_created=number_of_chunks,
         message="Documento processado e indexado com sucesso."
     )
 
+    except Exception as e:
+
+        print(f"Erro na Ingestão: {str(e)}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail=f"Erro interno ao processar documento: {str(e)}"
+        )
+
+    
 
 @router.post("/retrieve", response_model=RetrievalResponse)
 async def retrieve_document(request: RetrievalRequest):
--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Demonstração RAG"
-  
+
+    EMBEDDING_MODEL_NAME: str = "google/embeddinggemma-300m"
+
+    HF_TOKEN: str
+
+    FAISS_INDEX_PATH: str = "faiss_index_store"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
 
 @lru_cache()
 def get_settings():
